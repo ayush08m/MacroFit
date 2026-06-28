771e392deb2decd7181ee8ff843f181bc92b3953
ayush08m <ayush08am@gmail.com>
2026-06-29 00:35:06 +0530

    first commit

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,43 @@
+import HomeHeader from '@/components/HomeHeader';
+import MacroGrid from '@/components/MacroGrid';
+import RecentMeals from '@/components/RecentMeals';
+import { getMeals, Meal } from '@/storage/meals';
+import { globalStyles } from '@/styles/global';
+import { useFocusEffect } from 'expo-router';
+import { useCallback, useState } from 'react';
+import { ScrollView, Text, View } from 'react-native';
+import ShareButton from '@/components/ShareButton';
+import CopyButton from '@/components/CopyButton';
+// import ReminderToggle from '@/components/ReminderToggle';
+
+
+export default function HomeScreen() {
+  const [meals, setMeals] = useState<Meal[]>([]);
+
+  const loadMeals = async () => {
+    const data = await getMeals();
+    setMeals(data);
+    console.log('Loaded meals:', data);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      loadMeals();
+    }, []),
+  );
+
+  return (
+    <ScrollView style={globalStyles.container}>
+      <View style={globalStyles.header}>
+        <Text style={globalStyles.title}>MacroFit</Text>
+         <ShareButton meals={meals} />
+      </View>
+      <HomeHeader />
+      <MacroGrid meals={meals}/>
+      <CopyButton meals={meals} />
+      {/* <ReminderToggle/> */}
+      <RecentMeals meals={meals} onDelete={loadMeals} />
+
+    </ScrollView>
+  );
+}
